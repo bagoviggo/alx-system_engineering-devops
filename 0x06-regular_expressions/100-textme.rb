270c70c00433186f5c2a5cb1559d7f37784d1e14
bagoviggo <bagoviggo@gmail.com>
2023-02-02 21:42:06 +0300

    Power Sub

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # Script that output SENDER, RECEIVER and FLAGS
-$stdout.puts ARGV[0].scan(/(?<=from:|to:|flags:)[^\]]*/).join(,)"\n"
+puts ARGV[0].scan(/(?<=from:|to:|flags:)[^\]]*/).join
