c9d30a2398d6f6e5c5c5142e9e1419a2cc05f2ee
bagoviggo <bagoviggo@gmail.com>
2023-04-25 04:19:58 +0300

    Add dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+
+"""
+This script retrieves information from a REST API to display the progress of an employee's TODO list.
+It takes an employee ID as a command-line argument and displays the number of completed tasks
+out of the total tasks, along with the titles of the completed tasks.
+
+Usage:
+    python3 0-gather_data_from_an_API.py <employee_id>
+
+Args:
+    employee_id (int): The ID of the employee
+
+Returns:
+    None
+"""
+
+import requests
+import sys
+import json
+
+# Base URL for the REST API
+BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+
+def get_employee_todo_progress(employee_id):
+    """
+    Get the name of an employee
+
+    Args:
+        user_id (int): The user id of the employee
+
+    Returns:
+        str: The name of the employee
+    """
+    employee_info_url = f'{BASE_URL}/users/{employee_id}'
+    employee_info = requests.get(employee_info_url).json()
+
+    # Get employee's TODO list
+    employee_todo_url = f'{BASE_URL}/todos?userId={employee_id}'
+    employee_todo_list = requests.get(employee_todo_url).json()
+
+    # Count the number of completed tasks
+    completed_tasks = [
+        task for task in employee_todo_list if task.get('completed', False)
+    ]
+    num_completed_tasks = len(completed_tasks)
+
+    # Calculate the total number of tasks
+    total_tasks = len(employee_todo_list)
+
+    # Display employee TODO list progress
+    print(f"Employee {employee_info['name']} is done with tasks"
+          f"({num_completed_tasks}/{total_tasks}):")
+
+    # Display completed tasks
+    if num_completed_tasks > 0:
+        for task in completed_tasks:
+            print(f"\t {task['title']}")
+
+
+if __name__ == "__main__":
+    # Check if employee ID is provided as a command-line argument
+    if len(sys.argv) == 2:
+        employee_id = sys.argv[1]
+        # Validate command-line argument for integer employee ID
+        if employee_id.isdigit():
+            get_employee_todo_progress(int(employee_id))
+        else:
+            print("Invalid employee ID. Please enter a valid integer.")
+    else:
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+
+    # Retrieve data for all employees
+    all_employee_data = {}
+    for employee_id in range(1, 11):
+        employee_todo_url = f'{BASE_URL}/todos?userId={employee_id}'
+        employee_todo_list = requests.get(employee_todo_url).json()
+
+        # Get employee info
+        employee_info_url = f'{BASE_URL}/users/{employee_id}'
+        employee_info = requests.get(employee_info_url).json()
+        username = employee_info['username']
+
+        # Store tasks for employee
+        tasks = []
+        for task in employee_todo_list:
+            task_data = {
+                'username': username,
+                'task': task['title'],
+                'completed': task['completed']
+            }
+            tasks.append(task_data)
+
+        # Store tasks for employee in all_employee_data
+        all_employee_data[str(employee_id)] = tasks
+
+    # Export data to JSON
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(all_employee_data, json_file)
