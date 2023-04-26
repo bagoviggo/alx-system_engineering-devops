75e9f67cd5c4ffd4bc5cc25305ac68fd2210eb1c
bagoviggo <bagoviggo@gmail.com>
2023-04-27 02:30:16 +0300

    fix task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -22,9 +22,19 @@ if __name__ == '__main__':
             tasks = list(filter(lambda x: x.get('userId') == id, task_req))
             completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
             with open('{}.csv'.format(id), mode='w') as file:
-                writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+                writer = csv.writer(
+                                    file,
+                                    delimiter=','
+                                    quotechar='"'
+                                    quoting=csv.QUOTE_ALL
+                            )
                 for task in tasks:
-                    writer.writerow([id, username, task.get('completed'), task.get('title')])
+                    writer.writerow([
+                                    id
+                                    username
+                                    task.get('completed')
+                                    task.get('title')
+                                    ])
             print(
                 'Employee {} is done with tasks({}/{}):'.format(
                     emp_name,
@@ -35,4 +45,3 @@ if __name__ == '__main__':
             if len(completed_tasks) > 0:
                 for task in completed_tasks:
                     print('\t {}'.format(task.get('title')))
-
