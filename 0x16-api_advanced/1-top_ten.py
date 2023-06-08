41e95bc81b45917e78ca45c9978a4747106d148a
bagoviggo <bagoviggo@gmail.com>
2023-06-08 17:05:48 +0300

    Add 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" This module contains function that Queries the Reddit API
+    that returns top 10 posts from reddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+
+    Prints:
+        The titles of the first 10 hot posts in the subreddit.
+
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Alx-reddit/1.0"}  # Set a custom User-Agent header
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            posts = data["data"]["children"]
+
+            for post in posts:
+                title = post["data"]["title"]
+                print(title)
+
+        except KeyError:
+            print("Invalid subreddit.")
+    else:
+        print("Invalid subreddit.")
